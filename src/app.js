59e32b76ba46cd7483b57e76b6f1dac604e00a73
Vitor-Manoel565 <vitoralvesbvgs@gmail.com>
2022-11-24 14:10:14 -0300

    env

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express,{json} from 'express';
 import cors from 'cors';
 import routes from './routes';
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 
 
 const app = express()
@@ -9,7 +11,7 @@ app.use(cors())
 app.use(json())
 app.use(routes)
 
-app.listen(4000, () => {
+app.listen(PORT, () => {
   console.log('Ouvindo a porta 4000');
 });
 
