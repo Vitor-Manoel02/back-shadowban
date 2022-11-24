c1575d33d26ee0d2267ca8ffe571a4d563df0f5e
Vitor-Manoel565 <vitoralvesbvgs@gmail.com>
2022-11-24 14:15:10 -0300

    env

--- src/app.js
+++ src/app.js
@@ -1,18 +1,14 @@
-import express,{json} from 'express';
+import express, { json } from 'express';
 import cors from 'cors';
 import routes from './routes';
-import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config()
+import 'dotenv/config';
 
+const app = express();
 
-const app = express()
+app.use(cors());
+app.use(json());
+app.use(routes);
 
-app.use(cors())
-app.use(json())
-app.use(routes)
-
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log('Ouvindo a porta 4000');
 });
-
-
